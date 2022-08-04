d813f2287881d4158d3538f8d983f1ffe3e5440c
myller <myller42@gmail.com>
2022-08-04 16:10:13 -0300

    Alteração projeto CalcularImc

--- CalculoIMC/Program.cs
+++ CalculoIMC/Program.cs
@@ -9,7 +9,12 @@ namespace CalculoIMC
     internal class Program
     {
         private const string LER_PESO = "Peso: ",
-                             LER_ALTURA = "Altura: ";
+                             LER_ALTURA = "Altura: ",
+                             PESO_BAIXO = "Abaixo do peso",
+                             PESO_SAUDAVEL = "Peso saudável",
+                             SOBREPESO = "Sobrepeso",
+                             OBESO = "Obeso",
+                             OBESO_MORBIDO = "Obeso mórbida";
 
         static void Main(string[] args)
         {
@@ -20,8 +25,21 @@ namespace CalculoIMC
         {
             float peso = lerNumeroReal(LER_PESO);
             float altura = lerNumeroReal(LER_ALTURA);
+            float imc = calcularIMC(peso, altura);
 
-            Console.WriteLine(calcularIMC(peso, altura));
+            if (imc < 18.5)
+                exibirMensagem(PESO_BAIXO);
+
+            else if (imc <= 24.9)
+                exibirMensagem(PESO_SAUDAVEL);
+
+            else if (imc <= 29.9)
+                exibirMensagem(SOBREPESO);
+
+            else if (imc <= 39.9)
+                exibirMensagem(OBESO);
+
+            else exibirMensagem(OBESO_MORBIDO);
         }
 
         private static float calcularIMC(float peso, float altura)
